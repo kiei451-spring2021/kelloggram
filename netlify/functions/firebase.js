ca32dfb97f63633d0b1e17e9d02bb705f18cd149
Brian Eng <brian@brianeng.com>
2021-05-26 16:36:41 -0500

    Update firebase.js

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,14 +1,7 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCGd7OAVUT1u3Sf9uoSh_6l8iHg2qh_JJ4",
-  authDomain: "kiei-451-spring-2021.firebaseapp.com",
-  projectId: "kiei-451-spring-2021",
-  storageBucket: "kiei-451-spring-2021.appspot.com",
-  messagingSenderId: "902466001150",
-  appId: "1:902466001150:web:b5a42e97ab21026b706daf"
-} // replace
+const firebaseConfig = {} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
